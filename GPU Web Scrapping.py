c43ce08078cebaf968b4c7c46b381f34d32b7d9a
Lorenzo Castellini Coutin <153740191+Lorenzo-Castellini-Coutin@users.noreply.github.com>
2024-05-29 17:29:25 -0400

    Add files via upload

--- GPU Web Scrapping.py
+++ GPU Web Scrapping.py
@@ -1,3 +1,5 @@
+#Author: Lorenzo Castellini Coutin
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import requests
@@ -6,10 +8,15 @@ import csv
 
 driver = webdriver.Chrome()
 
-URL = "https://www.amazon.com/"
+URL = 'https://www.newegg.com/p/pl?d=rtx+4070'
 
 driver.get(URL)
 
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+results = soup.find_all('div', {'class' : 'item-container'})
+
+print(results[0])
 
 
 
