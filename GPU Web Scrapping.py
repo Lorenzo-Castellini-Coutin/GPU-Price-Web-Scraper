652ec69b2de0e1de654e7c1ddd129d80f6936991
Lorenzo Castellini Coutin <153740191+Lorenzo-Castellini-Coutin@users.noreply.github.com>
2024-05-28 20:05:27 -0400

    Add files via upload

---
+++ GPU Web Scrapping.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import requests
+import pandas as pd
+import csv
+
+driver = webdriver.Chrome()
+
+URL = "https://www.amazon.com/"
+
+driver.get(URL)
+
+
+
+
+
