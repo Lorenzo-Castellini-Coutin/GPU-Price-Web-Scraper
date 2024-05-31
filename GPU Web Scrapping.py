67b9afae5e5a758937c61e992a8758540a0ae4fd
Lorenzo Castellini Coutin <153740191+Lorenzo-Castellini-Coutin@users.noreply.github.com>
2024-05-31 16:28:01 -0400

    Add files via upload

--- GPU Web Scrapping.py
+++ GPU Web Scrapping.py
@@ -5,6 +5,13 @@ from selenium import webdriver
 import requests
 import pandas as pd
 import csv
+import pyshorteners
+
+file_path ='GPU_Data.csv'
+
+GPU_list = []
+prices_list = []
+links_list = []
 
 driver = webdriver.Chrome()
 
@@ -17,7 +24,7 @@ soup = BeautifulSoup(driver.page_source, 'html.parser')
 GPUs = soup.find_all('a', {'class' : 'item-title'})
 
 for g in GPUs:
-    print(g.get_text())
+    GPU_list.append(g.get_text())
 
 prices = soup.find_all('li', {'class' : 'price-current'})
 
@@ -25,13 +32,24 @@ for p in prices:
     price_tag = p.find('strong')
 
     if price_tag:
-        print("$" + str(price_tag.get_text()))
+        prices_list.append("$" + str(price_tag.get_text()))
 
 links = soup.find_all('a', {'class' : 'item-title'})
 
 for l in links:
     full_links = l.get('href')
-    print(full_links)
+    shortener = pyshorteners.Shortener()
+    short_links = shortener.tinyurl.short(full_links)
+    links_list.append(short_links)
+
+
+with open(file_path, 'w', newline = '') as csv_file:
+    GPU_data = csv.writer(csv_file)
+    GPU_data.writerow(['Item', 'Price', 'Shop Links (Newegg.com)'])
+    
+    for g, p, l in zip(GPU_list, prices_list, links_list):
+        GPU_data.writerow([g, p, l])
+
 
 
 
