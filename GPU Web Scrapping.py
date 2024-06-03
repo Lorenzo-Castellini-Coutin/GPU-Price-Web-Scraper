8a1f3f286617717c2d4a04d337f74e4c3e42c695
Lorenzo Castellini Coutin <153740191+Lorenzo-Castellini-Coutin@users.noreply.github.com>
2024-06-03 12:46:14 -0400

    Add files via upload

--- GPU Web Scrapping.py
+++ GPU Web Scrapping.py
@@ -5,23 +5,64 @@ from selenium import webdriver
 import requests
 import pandas as pd
 import csv
+import pyshorteners
 
-driver = webdriver.Chrome()
+Basic_URL = 'https://www.newegg.com/p/pl?d=rtx+4070&page=1' #Newegg URL for RTX 4070 GPU
 
-URL = 'https://www.newegg.com/p/pl?d=rtx+4070'
+num_of_pages = int(input('Please enter the number of pages you want to search in Newegg: '))
 
-driver.get(URL)
+def web_scrape(Basic_URL, num_of_pages):
+    driver = webdriver.Chrome()
+    GPU_list = []
+    prices_list = []
+    links_list = []
 
-soup = BeautifulSoup(driver.page_source, 'html.parser')
+    web = requests.get('https://www.newegg.com/p/pl?d=rtx+4070&page=1')
+    if web.status_code == 200:
+        for pages in range(1, num_of_pages + 1):
+            pages_url = f'https://www.newegg.com/p/pl?d=rtx+4070&page={pages}'
+            driver.get(pages_url)
 
-GPUs = soup.find_all('div', {'class' : 'item-container'})
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-prices_div = soup.find('li', class_ = 'price-current')
+    
+            GPUs = soup.find_all('a', {'class' : 'item-title'})
 
-prices_res = prices_div.find_all('strong')
+            for g in GPUs:
+                GPU_list.append(g.get_text())
 
-for p in prices_res:
-    print(p.text)
+            prices = soup.find_all('li', {'class' : 'price-current'})
+
+            for p in prices:
+                price_tag = p.find('strong')
+
+                if price_tag:
+                    prices_list.append("$" + str(price_tag.get_text()))
+
+            links = soup.find_all('a', {'class' : 'item-title'})
+
+            for l in links:
+                full_links = l.get('href')
+                shortener = pyshorteners.Shortener()
+                short_links = shortener.tinyurl.short(full_links)
+                links_list.append(short_links)
+
+    else:
+        print('The website you are trying to access is not available. Try again later.')
+    
+    file_path = 'GPU_Scrapped_Data.csv'
+    
+    with open(file_path, 'a', newline = '') as csv_file:
+        GPU_data = csv.writer(csv_file)
+        GPU_data.writerow(['Item', 'Price', 'Newegg Shop Links'])
+
+        for g, p, l in zip(GPU_list, prices_list, links_list):
+            GPU_data.writerow([g, p, l])
+
+web_scrape(Basic_URL, num_of_pages)
+
+
+
 
 
 
